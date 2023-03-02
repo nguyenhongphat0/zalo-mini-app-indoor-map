9f666253d0c9d0e930c80c51923a16b3f704bab2
nguyenhongphat0 <hongphat.js@gmail.com>
2023-03-02 15:51:17 +0700

    get phone number

--- src/components/indoor-map/user-ticket.tsx
+++ src/components/indoor-map/user-ticket.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { FC } from "react";
 import { useRecoilValue } from "recoil";
+import { getPhoneNumber } from "zmp-sdk";
 import { Avatar } from "zmp-ui";
 import { userState } from "../../state";
 
 export const UserTicket: FC = () => {
   const user = useRecoilValue(userState);
+  const buy = () => {
+    getPhoneNumber({});
+  }
   return (
-    <div className="rounded-2xl bg-black/80 text-white flex gap-4 p-2 items-center">
+    <div onClick={buy} className="rounded-2xl bg-black/80 text-white flex gap-4 p-2 items-center">
       <Avatar online src={user.avatar} />
       <p>
         <b>{user.name}</b><br />
